f9897c0b9ee3fc313748ac731cb8f5b56a0b06d6
Sara Chehab <sara@chehab.co>
2023-10-30 23:56:56 +0000

    Initial Commit

---
+++ tcpclient(1).py
@@ -0,0 +1,29 @@
+import socket
+#the server name and port
+#the server is on the same computer as the client
+
+server_name = 'localhost'
+server_port = 12000
+
+#create a TCP client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Set up a TCP connection with the server
+
+#a connection_socket will be assigned to this client on the server side
+
+client_socket.connect((server_name, server_port))
+
+print("TCP client running...")
+print("Connecting to server at IP: ", server_name, " PORT: ", server_port)
+
+#take input from the user
+msg = input("Enter a string to test if it is alphanumeric: ");
+
+#send the message  to the udp server
+client_socket.send(msg.encode())
+
+#return values from the server
+msg = client_socket.recv(1024)
+print(msg.decode())
+client_socket.close()
---
+++ updserver.py
@@ -0,0 +1,28 @@
+import socket
+
+#select a server port
+server_port = 12000
+
+#create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#bind the server to the localhost at port server_port
+server_socket.bind(('',server_port))
+
+print('UDP Server running on port ', server_port)
+
+#Now the loop that listens from clients
+#As UDP is not connection oriented,the same UDP socket serves all clients
+
+while True:
+    #cadd below is the client process address
+    cmsg, cadd = server_socket.recvfrom(2048)
+    cmsg = cmsg.decode()
+    if(cmsg.isalnum()==False):
+        cmsg = "Not alphanumeric."
+    else:
+        cmsg = "Alphanumeric."
+
+    #send reply message to the client process            
+    server_socket.sendto(cmsg.encode(),cadd)
+
